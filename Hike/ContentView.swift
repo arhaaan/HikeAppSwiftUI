905721c58916a22bce45cc3aa80683ec2d7a0fe8
arhaaan <85577285+arhaaan@users.noreply.github.com>
2025-03-13 16:20:37 +0700

    Add files via upload

---
+++ Hike/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Karim Arhan on 13/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Karim Arhan on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
