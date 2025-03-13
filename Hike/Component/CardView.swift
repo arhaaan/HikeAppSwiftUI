a374ba0307361fd6a0297362b71cc92cbc1eb4c0
Karim Arhan <karim.arhan@yahoo.com>
2025-03-13 22:08:15 +0700

    create a custom button style make a random number func to change image

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct CardView: View {
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
         // MARK: - CARD
-        
         ZStack {
             CustomBackgroundView()
             
@@ -61,13 +71,27 @@ struct CardView: View {
                         )
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.easeInOut(duration: 1), value: imageNumber)
                     
                 }
                 
                 // MARK: - FOOTER
+                Button {
+                    //ACTION: Generate a random number
+                    print("Button pressed")
+                    randomImage()
+                } label: {
+                    Text("Explore More!")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight,.customGreenMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
                 
             } //: VSTACK
         } //: CARD
